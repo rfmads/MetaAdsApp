b65271adf490f25cd1a16dc8cbbe653baf2a54d0
Amal-Irshiad <amalirshiad97@gmail.com>
2026-02-07 16:41:44 +0530

    Ignore config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,13 @@
-# config/config.py
 import os
 
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "s1s2s3s4s5")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME", "MetaAdsdb")
+
+if not DB_PASSWORD:
+    raise RuntimeError("DB_PASSWORD is missing. Set it in .env file.")
 
 META_USER_TOKEN = os.getenv("META_USER_TOKEN")
 META_GRAPH_VERSION = os.getenv("META_GRAPH_VERSION", "v22.0")
